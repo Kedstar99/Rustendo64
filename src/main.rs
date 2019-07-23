2db9ddd4191def263db44db82983e008b8d1a9d5
Krish De Souza <K.DeSouza@Outlook.com>
2019-07-24 00:35:21 +0100

    First attempt at argument parsing which will be used to later to read a ROM hopefully

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(myapp =>
+        (version: "0.1.0")
+        (author: "Krish De Souza <K.DeSouza@Outlook.com>")
+        (about: "Rustendo64 emulator attempt")
+        (@arg CONFIG: -f --filename +takes_value "Reads a ROM file")
+    ).get_matches();
+    let config = matches.value_of("config");
+    println!("Value for config: {:?}", config)
 }
