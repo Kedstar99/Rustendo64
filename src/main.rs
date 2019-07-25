3ddae2fe74053a209609630120dfcb87b1c74afc
Krish De Souza <K.DeSouza@Outlook.com>
2019-07-25 18:29:47 +0100

    Added file reader to read ROM into buffer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 #[macro_use]
 extern crate clap;
+use std::fs;
+use std::io::Read;
+
 
 fn main() {
     let matches = clap_app!(myapp =>
         (version: "0.1.0")
         (author: "Krish De Souza <K.DeSouza@Outlook.com>")
         (about: "Rustendo64 emulator attempt")
-        (@arg CONFIG: -f --filename +takes_value "Reads a ROM file")
+        (@arg file_path: -f --filepath +takes_value "Filepath to to File name")
     ).get_matches();
-    let config = matches.value_of("config");
-    println!("Value for config: {:?}", config)
+    let file_path = matches.value_of("file_path").unwrap();
+    let mut file = fs::File::open(file_path).unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf).unwrap();
+    let file_buf = file_buf;
+    println!("Value for config: {:?}", file_buf)
 }
