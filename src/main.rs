90b884d185e5290b6c236eee2fbd40cfa58158a1
Krish De Souza <K.DeSouza@Outlook.com>
2019-07-28 23:24:18 +0100

    Abstracted away ability to load ROM files, also added addition flat for PIF BIOS ROM

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate clap;
 use std::fs;
 use std::io::Read;
+use std::path::Path;
 
 
 fn main() {
@@ -9,12 +10,20 @@ fn main() {
         (version: "0.1.0")
         (author: "Krish De Souza <K.DeSouza@Outlook.com>")
         (about: "Rustendo64 emulator attempt")
-        (@arg file_path: -f --filepath +takes_value "Filepath to to File name")
+        (@arg rom_path: -f --filepath +takes_value "Filepath to ROM")
+        (@arg pif_path: -p --piopath +takes_value "Filepath to PIF ROM")
     ).get_matches();
-    let file_path = matches.value_of("file_path").unwrap();
-    let mut file = fs::File::open(file_path).unwrap();
+    let file_path = matches.value_of("rom_path").unwrap();
+    let pif_path = matches.value_of("pif_path").unwrap();
+    let file_buf = load_bin(Path::new(file_path));
+    let pif_buf = load_bin(Path::new(pif_path));
+    println!("Value for ROM: {:x?}", file_buf);
+    println!("Value for PIF: {:?}", pif_buf)
+}
+
+fn load_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = fs::File::open(path.as_ref()).unwrap();
     let mut file_buf = Vec::new();
     file.read_to_end(&mut file_buf).unwrap();
-    let file_buf = file_buf;
-    println!("Value for config: {:?}", file_buf)
+    file_buf
 }
