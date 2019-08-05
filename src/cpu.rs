45b461c7473d373901330b06f669833c893ee0bc
Krish De Souza <K.DeSouza@Outlook.com>
2019-08-05 22:49:13 +0100

    Moved CPU module to separate file

---
+++ src/cpu.rs
@@ -0,0 +1,80 @@
+
+//see Google drive for CPU spec
+#[derive(Default)]
+pub struct Cpu { 
+    gpr: [u64; 32],
+    fpr: [f64; 32],
+    pc: u64,
+    high: u64,
+    low: u64,
+    llbit: bool,
+    fcr0: u32,
+    fcr32: u32,
+
+    cp0:CP0,
+}
+
+impl Cpu {
+    pub fn new() -> Self {
+        Cpu::default()
+    }
+
+    pub fn power_on_reset(&mut self) {
+        self.cp0.power_on_reset()
+    }
+}
+
+enum RegConfigEp {
+    D,
+    DxxDxx,
+    RFU
+}
+
+impl Default for RegConfigEp {
+    fn default() -> RegConfigEp {
+        RegConfigEp::D
+    }
+
+}
+
+enum RegConfigBe {
+    LittleEndian,
+    BigEndian
+}
+
+impl Default for RegConfigBe {
+    fn default() -> RegConfigBe {
+        RegConfigBe::BigEndian
+    }
+}
+
+
+#[derive(Default)]
+struct RegConfig {
+    reg_config_ep: RegConfigEp,
+    reg_config_be: RegConfigBe
+}
+
+impl RegConfig {
+    fn power_on_reset(&mut self) {
+        self.reg_config_ep = RegConfigEp::D;
+        self.reg_config_be = RegConfigBe::BigEndian;
+    }
+}
+
+
+// Coprocessor 0
+#[derive(Default)]
+struct CP0 {
+    reg_config: RegConfig
+}
+
+impl CP0 {
+    fn new()->Self{
+        CP0::default()
+    }
+
+    fn power_on_reset(&mut self) {
+        self.reg_config.power_on_reset();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,70 +4,7 @@ use std::fs;
 use std::io::Read;
 use std::path::Path;
 
-//see Google drive for CPU spec
-#[derive(Default)]
-struct Cpu { 
-    gpr: [u64; 32],
-    fpr: [f64; 32],
-    pc: u64,
-    high: u64,
-    low: u64,
-    llbit: bool,
-    fcr0: u32,
-    fcr32: u32,
-
-    cp0:CP0,
-}
-
-impl Cpu {
-    fn new() -> Self {
-        Cpu::default()
-    }
-
-    fn power_on_reset(&mut self) {
-        self.cp0.power_on_reset()
-    }
-}
-
-// Coprocessor 0
-#[derive(Default)]
-struct CP0 {
-    index: u64,
-    random: u64,
-    entry_lo0: u64,
-    entry_lo1: u64,
-    context: u64,
-    page_mask: u64,
-    wired: u64,
-    bad_v_addr: i64,
-    count: u64,
-    entry_hi: u64,
-    compare: u64,
-    status: u64,
-    cause: u64,
-    epc: u64,
-    prid: u64,
-    config: u64,
-    lladdr: u64,
-    watchlo: u64,
-    watchhi: u64,
-    xcontext: u64,
-    parity_err: u64,
-    cache_err: u64,
-    tag_lo: u64,
-    tag_hi: u64,
-    epc_err: u64
-}
-
-impl CP0 {
-    fn new()->Self{
-        CP0::default()
-    }
-
-    fn power_on_reset(&mut self) {
-        
-    }
-}
+mod cpu;
 
 fn main() {
     let matches = clap_app!(myapp =>
@@ -82,7 +19,9 @@ fn main() {
     let file_buf = load_bin(Path::new(file_path));
     let pif_buf = load_bin(Path::new(pif_path));
     println!("Value for ROM: {:x?}", file_buf);
-    println!("Value for PIF: {:?}", pif_buf)
+    println!("Value for PIF: {:?}", pif_buf);
+    let mut cpu = cpu::Cpu::new();
+    cpu.power_on_reset();
 }
 
 fn load_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
