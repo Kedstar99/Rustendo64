269c5c430274a47221a7964f112358699e03e28d
Krish De Souza <K.DeSouza@Outlook.com>
2019-08-22 00:53:17 +0100

    Figured out how to convert from vec into box

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -2,14 +2,14 @@ const RAM_SIZE: usize = 4 * 1024 * 1024;
 
 pub struct Interconnect {
     pif_rom: Vec<u8>,
-    ram: Vec<u16>
+    ram: Box<[u16]>
 }
 
 impl Interconnect {
     pub fn new(pif_rom: Vec<u8>) -> Self {
         Interconnect{
             pif_rom: pif_rom,
-            ram: vec![0; RAM_SIZE]
+            ram: vec![0; RAM_SIZE].into_boxed_slice()
         }
     }
 
