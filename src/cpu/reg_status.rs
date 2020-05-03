9005ebed7a9334023a700d6cf845128a075105cf
Krish De Souza <K.DeSouza@Outlook.com>
2020-05-03 02:00:36 +0100

    Fixed compiler issues and added KSU / Interrupt struct

--- src/cpu/reg_status.rs
+++ src/cpu/reg_status.rs
@@ -17,19 +17,37 @@ struct DiagnosticStatus {
     condition_bit: bool, //CH
 }
 
+#[derive(Debug, Default)]
+struct InterruptMask {
+    enabled: bool,
+    timer_interrupt: bool, //IM(7)
+    external_interrupts_or_write_requests: [bool; 5], //IM(6, 2)
+    software_interrupt_cause_reg: [bool; 2] //IM(1, 0)
+}
+
+#[derive(Debug)]
+enum Mode {
+    Kernel, // 00
+    Supervisor, // 01
+    User,  // 10
+}
+
+impl Default for Mode {
+    fn default() -> Self {Mode::Kernel}
+}
 
 #[derive(Debug, Default)]
 pub struct RegStatus {
     // Page 166 User manual Exception Processoring Register
-    coprocessor_usability: [bool, 4], //CU
+    coprocessor_usability: [bool; 4], //CU
     low_power: bool, //RP
     additional_fp_regs: bool, //FR
     reverse_endian: bool, //RE
-    diagnostic_status: DiagnosticsStatus, //DS
+    diagnostic_status: DiagnosticStatus, //DS
     interrupt_mask: InterruptMask, //IM
     kernel_mode_64bit_addr: bool, //KX
     supervisor_mode__64bit_addr_: bool, //SX
-    user_mode__64bit_addr: bool  //UX
+    user_mode__64bit_addr: bool,  //UX
     mode: Mode, //KSU
     error_level: bool, //ERL
     exception_level: bool, //EXL
